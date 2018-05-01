bcfd4e16a2f6c97a0fae1cb395164c5b455a491a
hakeempolistico <hjpolistico@gmail.com>
2018-05-01 16:03:11 +0800

    schema update

--- schema/bms.sql
+++ schema/bms.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2018 at 04:43 PM
--- Server version: 5.7.11
--- PHP Version: 7.0.4
+-- Generation Time: May 01, 2018 at 10:02 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -45,7 +47,32 @@ CREATE TABLE `bills` (
 INSERT INTO `bills` (`invoice_number`, `company`, `service`, `period_start`, `period_end`, `amount`, `date_paid`, `amount_paid`, `date_added`) VALUES
 ('1', 'Meralco', 'Electricity', '2018-04-01', '2018-05-02', '5000.00', '2018-04-27', '5000.00', '2018-04-28 16:39:55'),
 ('2', 'Maynilad', 'Water', '2018-04-11', '2018-05-11', '2000.00', '0000-00-00', '0.00', '2018-04-27 20:18:53'),
-('3', 'Meralco', 'Electricity', '2018-05-01', '2018-06-01', '5000.00', '0000-00-00', '0.00', '2018-04-27 20:22:26');
+('3', 'Meralco', 'Electricity', '2018-05-01', '2018-06-01', '5000.00', '0000-00-00', '0.00', '2018-04-27 20:22:26'),
+('4', 'Meralco', 'Electricity', '2018-05-01', '2018-06-01', '5000.00', '0000-00-00', '0.00', '2018-05-01 01:24:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `user_type` enum('admin','member') NOT NULL,
+  `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `email`, `user_type`, `date_added`) VALUES
+(1, 'admin.hakeem', 'admin', 'Hakeem', 'Polistico', 'hakeem.polistico@tup.edu.ph', 'admin', '2018-05-01 04:04:46');
 
 --
 -- Indexes for dumped tables
@@ -56,6 +83,23 @@ INSERT INTO `bills` (`invoice_number`, `company`, `service`, `period_start`, `pe
 --
 ALTER TABLE `bills`
   ADD PRIMARY KEY (`invoice_number`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
